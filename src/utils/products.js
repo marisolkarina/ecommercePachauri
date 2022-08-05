c952e4c94504ebf94eaf2d4a3fb1a7bfa35e1fa1
marisolkarina <marisol.kari.20@gmail.com>
2022-08-04 21:55:33 -0500

    imagenes en firebase storage

--- src/utils/products.js
+++ src/utils/products.js
@@ -2,101 +2,91 @@ export const data = [
     {
         id: 1,
         nombre: "Paleta de Sombras Paradise Enchanted 0.25 fl oz",
-        imagen: 'https://falabella.scene7.com/is/image/FalabellaPE/16555259_1?wid=800&hei=800&qlt=70',       
+        imagen: 'https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/1.jpg?alt=media&token=b7e71d63-63b4-4061-b71b-f778ca2668cb',       
         descripcion: "Esta paleta de sombras cuenta con 12 tonos cálidos y cautivadores, incluyendo marfil cremoso, nudos rosados, ámbar bronceado y toques de rosa coral. Está perfumada e impregnada con un aroma a fruta encantadora para una experiencia sensorial definitiva.",
         precio: 100,
         stock: 6,
-        categoryId: "maquillaje",
-        productId: 1
+        categoryId: "maquillaje"
     },
     {
         id: 2,
         nombre: "Agua micelar desmaquillante Piel mixta a grasas 200 ml",
-        imagen: 'https://falabella.scene7.com/is/image/FalabellaPE/16022466_1?wid=1500&hei=1500&qlt=70',       
+        imagen: 'https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/2.jpg?alt=media&token=5f427c08-1548-4256-b2b7-34da1f4918b0',       
         descripcion: "El Agua Micelar limpia y remueve eficazmente el maquillaje del rostro, ojos y labios, sin enjuagar y sin sensación grasosa. Además, equilibra y matifica la piel, ideal para pieles mixtas a grasas.",
         precio: 60,
         stock: 6,
-        categoryId: "cuidadodelrostro",
-        productId: 2
+        categoryId: "cuidadodelrostro"
     },
     {
         id: 3,
         nombre: "Fotoprotector Fusion Water SPF50",
-        imagen: 'https://s3.amazonaws.com/imagenes-sellers-mercado-ripley/2022/05/12133131/DER-84294201959501.jpg',       
+        imagen: 'https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/3.jpg?alt=media&token=7669b7f7-382e-44e3-a09d-1a403ba558f4',       
         descripcion: "Obtén una piel sana y libre de impurezas con este increíble producto Fotop Fusion Water Fps50 Fco X 50 que la marca ISDIN tiene para ti. ¡No dejes de tenerlo y resalta tu belleza siempre!",
         precio: 109,
         stock: 5,
-        categoryId: "dermocosmetica",
-        productId: 3
+        categoryId: "dermocosmetica"
     },
     {
         id: 4,
         nombre: "L Interdit  Edp 50 ml",
-        imagen: 'https://home.ripley.com.pe/Attachment/WOP_5/2014235573152/2014235573152_2.jpg',
+        imagen: 'https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/4.jpg?alt=media&token=92786acb-9d87-4419-95ba-c15b26358043',
         descripcion: "Givenchy presenta su icónica fragancia L¿Interdit. Un homenaje a la feminidad audaz. Una invitación a desafiar las convenciones y abrazar su singularidad. Un acorde floral blanco con notas oscuras, desatando una luminosidad audaz y elegante que coquetea con la oscuridad. Una nueva unión para sentir la atracción de lo prohibido.",
         precio: 320,
         stock: 10,
-        categoryId: "perfumes",
-        productId: 4
+        categoryId: "perfumes"
     },
     {
         id: 5,
         nombre: "OPI Duo Nude Dulce de Leche + Throw Me a Kiss",
-        imagen: 'https://falabella.scene7.com/is/image/FalabellaPE/19062134_1?wid=1500&hei=1500&qlt=70',
+        imagen: 'https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/5.jpg?alt=media&token=d48dfa1e-6e1c-4f3d-9d2d-656d7f925ba1',
         descripcion: "Givenchy presenta su icónica fragancia L¿Interdit. Un homenaje a la feminidad audaz. Una invitación a desafiar las convenciones y abrazar su singularidad. Un acorde floral blanco con notas oscuras, desatando una luminosidad audaz y elegante que coquetea con la oscuridad. Una nueva unión para sentir la atracción de lo prohibido.",
         precio: 150,
         stock: 3,
-        categoryId: "maquillaje",
-        productId: 5
+        categoryId: "maquillaje"
     },
     {
         id: 6,
         nombre: "Shampoo Purificante Cabello Graso Kerasilk",
-        imagen: 'https://falabella.scene7.com/is/image/FalabellaPE/17774032_1?wid=1500&hei=1500&qlt=70',
+        imagen: 'https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/6.jpg?alt=media&token=cfbcbc86-6d63-4b0e-85bd-cc46422932d5',
         descripcion: "Purificante para cabello graso y limpieza profunda con extracto de menta purifica tu cuero cabelludo y elimina exceso de grasa. sistema de detoxificación peeling capilar profundo. formulado con extractos orgánicos de menta, salvia, romero y la vitamina b3 o niacina. disfruta un cuero cabelludo completamente purificado y revitalizado. producto profesional para cuidado en casa productos de la línea: shampoo 250ml.",
         precio: 47,
         stock: 8,
-        categoryId: "cuidadocapilar",
-        productId: 6
+        categoryId: "cuidadocapilar"
     },
     {
         id: 7,
         nombre: "Emulsion Hidratante",
-        imagen: "https://falabella.scene7.com/is/image/FalabellaPE/17423271_1?wid=1500&hei=1500&qlt=70",
+        imagen: "https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/7.jpg?alt=media&token=7b570c06-36d4-4e76-b4f4-f8511347e3f5",
         descripcion: "Para el tratamiento completo de la piel sensible, utilizar conjuntamente con la línea de dermolimpiadores Cetaphil. Uso externo. Mantener fuera del alcance de los niños.",
         precio: 120,
         stock: 15,
-        categoryId: "dermocosmetica",
-        productId: 7
+        categoryId: "dermocosmetica"
     },
     {
         id: 8,
         nombre: "Davidoff Cool Water Sea Rose Eau de Toilette 50 ml",
-        imagen: "https://falabella.scene7.com/is/image/FalabellaPE/19045837_1?wid=1500&hei=1500&qlt=70",
+        imagen: "https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/8.jpg?alt=media&token=b4290d30-44fd-41a7-b666-f7eaae68553f",
         descripcion: "Experimente el eau de toilette Cool Water Woman Sea Rose de Davidoff. Descubre una fragancia para mujer inspirada en la belleza de las flores al amanecer. Cool Water Woman Sea Rose, una celebración de la feminidad, la frescura y la elegancia.",
         precio: 210,
         stock: 20,
         categoryId: "perfumes",
-        productId: 8
     },
     {
         id: 9,
         nombre: "Mascarilla Facial Peel-Off Cosmica Holografica Luminizacion Cuarzo Rosa 10ml",
-        imagen: "https://falabella.scene7.com/is/image/FalabellaPE/17796420_1?wid=1500&hei=1500&qlt=70",
+        imagen: "https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/9.jpg?alt=media&token=c5a2c207-20b9-4319-9c17-f6d6e9787192",
         descripcion: "Esta mascarilla peel-off holográfica inspirada en el cristal infundida con Cuarzo Rosa la 'piedra del amor' elimina las impurezas e ilumina para un cutis más brillante. Perfecto para todos los tipos de piel.",
         precio: 10,
         stock: 18,
-        categoryId: "cuidadodelrostro",
-        productId: 9
+        categoryId: "cuidadodelrostro"
     },
     {
         id: 10,
         nombre: "Tratamiento Milagroso 20 En 1 Head Muk 200Ml",
-        imagen: "https://falabella.scene7.com/is/image/FalabellaPE/17229144_1?wid=1500&hei=1500&qlt=70",
+        imagen: "https://firebasestorage.googleapis.com/v0/b/ecomm-sagitario.appspot.com/o/10.jpg?alt=media&token=8c3c0dc0-2892-4ed6-abb0-47da0b2590f5",
         descripcion: "Crema ligera, sin enjuague que otorga 20 beneficios al cabello. Dirigido para todo tipo de cabellos. Su alto factor de protector solar protege del agua salina, del cloro y el daño ambiental. Fragancia de maracuyá y sandía.",
         precio: 10,
         stock: 8,
-        categoryId: "cuidadocapilar",
-        productId: 10
+        categoryId: "cuidadocapilar"
     }
 ]
